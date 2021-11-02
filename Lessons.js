93c09cbc833dd3b2ba36e8eec60596d09101ff01
Dominique Walker <DW629@live.mdx.ac.uk>
2021-11-02 12:23:54 +0000

    Created the JSON objects

---
+++ Lessons.js
@@ -0,0 +1,57 @@
+
+
+var objectarray = [
+    
+    {
+        
+        "id": 101, "Subject":"Maths", "Price":"£30", "Location":"Wembly"
+    
+    } ,
+
+{
+    
+   "id": 102, "Subject":"French", "Price":"£35", "Location":"Wembly"
+
+
+},
+
+{
+    
+    "id": 201, "Subject":"Art", "Price":"£20", "Location":"Neasden"
+
+
+},
+
+{
+    "id": 301, "Subject" : "Spanish", "Price":"£35", "Location":"Wembly"
+
+},
+
+{ 
+    "id": 401, "Subject":"Art", "Price":"£30", "Location":"Wilesden Green"
+
+},
+
+{ "id": 402, "Subject":"English", "Price":"£30", "Location":"Wilesden Green"
+
+},
+
+{ 
+    "id" : 403, "Subject":"Geography", "Price":"£20", "Location":"Wilesden Green"
+
+},
+
+{ "id" : 501, "Subject":"History", "Price":"20", "Location":"Kilburn"
+
+},
+
+{"id" : 502, "Subject":"Art", "Price":"£30", "Location":"Kilburn"
+},
+
+{
+    "id" : 503, "Subject":"Maths", "Price":"£30", "Location":"Kilburn"
+
+},
+
+]
+
