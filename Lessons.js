372e04360870c9740e43105770e18bbcf4d33050
Dominique Walker <dw629@live.mdx.ac.uk>
2021-11-09 16:01:06 +0000

    Images added to products

--- Lessons.js
+++ Lessons.js
@@ -1,57 +1,103 @@
 
 
-var objectarray = [
+ let lessons = [
     
     {
         
-        "id": 101, "Subject":"Maths", "Price":"£30", "Location":"Wembly"
+        "id": 101, 
+        "Subject":"Maths",
+        "Price":"£30",
+         "Location":"Wembly",
+         "Rating": 4,
+         "Image": "maths.png"
     
     } ,
 
 {
     
-   "id": 102, "Subject":"French", "Price":"£35", "Location":"Wembly"
+   "id": 102, 
+   "Subject":"French",
+    "Price":"£35",
+   "Location":"Wembly",
+   "Image": "french.png"
 
 
 },
 
 {
     
-    "id": 201, "Subject":"Art", "Price":"£20", "Location":"Neasden"
+    "id": 201, 
+    "Subject":"Art", 
+    "Price":"£20", 
+    "Location":"Neasden",
+    "Image": "art.jpg"
 
 
 },
 
 {
-    "id": 301, "Subject" : "Spanish", "Price":"£35", "Location":"Wembly"
+    "id": 301,
+     "Subject" : "Spanish",
+     "Price":"£35",
+     "Location":"Wembly",
+     "Image": "spanish.png"
+
 
 },
 
 { 
-    "id": 401, "Subject":"Art", "Price":"£30", "Location":"Wilesden Green"
-
-},
-
-{ "id": 402, "Subject":"English", "Price":"£30", "Location":"Wilesden Green"
+    "id": 401, 
+    "Subject":"Art", 
+    "Price":"£30",
+    "Location":"Wilesden Green",
+    "Image": "art.jpg"
 
 },
 
 { 
-    "id" : 403, "Subject":"Geography", "Price":"£20", "Location":"Wilesden Green"
+    "id": 402, 
+"Subject":"English", 
+"Price":"£30",
+ "Location":"Wilesden Green",
+ "Image": "english.jpg"
+ 
 
 },
 
-{ "id" : 501, "Subject":"History", "Price":"20", "Location":"Kilburn"
+{ 
+    "id" : 403, 
+    "Subject":"Geography", 
+    "Price":"£20", 
+    "Location":"Wilesden Green",
+    "Image":"geography.png"
+
 
 },
 
-{"id" : 502, "Subject":"Art", "Price":"£30", "Location":"Kilburn"
+{ "id" : 501, 
+"Subject":"History", 
+"Price":"20", 
+"Location":"Kilburn",
+"Image": "history.png"
+
 },
 
 {
-    "id" : 503, "Subject":"Maths", "Price":"£30", "Location":"Kilburn"
+    "id" : 502,
+     "Subject":"Art", 
+     "Price":"£30",
+      "Location":"Kilburn",
+      "Image": "art.jpg"
+},
+
+{
+    "id" : 503, 
+    "Subject":"Maths", 
+    "Price":"£30",
+     "Location":"Kilburn",
+    "Image": "maths.png"
+     
 
 },
 
 ]
-
